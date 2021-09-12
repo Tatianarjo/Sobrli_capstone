2800e5f91941f43f924e829dc69e794733b320d1
Tatiana Johnson <Tatianarjo@gmail.com>
2021-09-12 11:22:05 -0500

    Working login and registration

--- src/components/Sobrli.js
+++ src/components/Sobrli.js
@@ -1,22 +1,45 @@
 import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 import "./Sobrli.css"
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import { NavBar } from "./nav/NavBar";
 import { ApplicationViews } from "./ApplicationViews";
 
 
 export const Sobrli = () => (
-    <>
-        <NavBar />
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("sobrli_user")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
+        
       
 
-        <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+     
 
-    <ApplicationViews />
-
-   
     
-    </>
-)
+
+    
+    
+
